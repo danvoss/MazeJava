aa64e9398ddee5068254aa807dd9b760d1ad626c
Dan <danielandrewvoss@gmail.com>
2016-07-13 11:25:12 -0400

    Finish maze

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -1,5 +1,9 @@
 package com.dvoss;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class Main {
 
     static final int SIZE = 10;
@@ -13,8 +17,73 @@ public class Main {
         return rooms;
     }
 
+    static ArrayList<Room> possibleNeighbors(Room[][] rooms, int row, int col) {
+        ArrayList<Room> neighbors = new ArrayList<>();
+        // get top room
+        if (row > 0) {
+            neighbors.add(rooms[row - 1][col]);
+        }
+        // get bottom room
+        if (row < SIZE - 1) {
+            neighbors.add(rooms[row + 1][col]);
+        }
+        // get left room
+        if (col > 0) {
+            neighbors.add(rooms[row][col - 1]);
+        }
+        // get right room
+        if (col < SIZE - 1) {
+            neighbors.add(rooms[row][col + 1]);
+        }
+        neighbors = neighbors.stream()
+                .filter(room -> !room.wasVisited)
+                .collect(Collectors.toCollection(ArrayList<Room>::new));
+        return neighbors;
+    }
+
+    static Room randomNeighbor(Room[][] rooms, int row, int col) {
+        ArrayList<Room> neighbors = possibleNeighbors(rooms, row, col);
+        if (neighbors.size() > 0) {
+            Random r = new Random();
+            int index = r.nextInt(neighbors.size());
+            return neighbors.get(index);
+        }
+        return null;
+    }
+
+    static void tearDownWall(Room oldRoom, Room newRoom) {
+        // going up
+        if (newRoom.row < oldRoom.row) {
+            newRoom.hasBottom = false;
+        }
+        //
+        else if (newRoom.row > oldRoom.row) {
+            oldRoom.hasBottom = false;
+        }
+        // going left
+        else if (newRoom.col < oldRoom.col) {
+            newRoom.hasRight = false;
+        }
+        // going right
+        else if (newRoom.col > oldRoom.col) {
+            oldRoom.hasRight = false;
+        }
+     }
+
+    static boolean createMaze(Room[][] rooms, Room room) {
+        room.wasVisited = true;
+        Room nextRoom = randomNeighbor(rooms, room.row, room.col);
+        if (nextRoom == null) {
+            return false;
+        }
+        tearDownWall(room, nextRoom);
+        while (createMaze(rooms, nextRoom)); // method call within method = recursion; while loop forces to visit every possible
+        return true;
+    }
+
     public static void main(String[] args) {
         Room[][] rooms = createRooms();
+        createMaze(rooms, rooms[0][0]);
         for (Room[] row : rooms) {
             System.out.print(" _");
         }
@@ -22,7 +91,8 @@ public class Main {
         for (Room[] row : rooms) {
             System.out.print("|");
             for (Room room : row) {
-                System.out.print("_|");
+                System.out.print(room.hasBottom ? "_" : " ");
+                System.out.print(room.hasRight ? "|" : " ");
             }
             System.out.println();
         }
